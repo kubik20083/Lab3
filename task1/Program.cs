758b73be48b3bb20aa7814cc5dc05a4fa8950a77
kubik <kubik20083@mail.ru>
2022-12-29 15:44:14 +0300

    Решение задачи, полиндром

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int x = number.Length;
+
+if (x == 5)
+{
+if (number[0] == number[4] && number[1] == number[3])
+{
+Console.WriteLine($"{number} - число палиндром");
+}
+else
+{
+Console.WriteLine($"{number} - число не палиндром");
+}
+}
+else
+{
+Console.WriteLine($"{number} - не является пятизначным числом");
+}
